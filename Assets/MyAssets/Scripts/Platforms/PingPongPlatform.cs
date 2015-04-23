7951e371b3082c8da276eb74f9601f48d54618c2
Eldir <lothvorn@gmail.com>
2015-04-23 21:49:11 +0200

    Añadida plataforma custom
    Añadido prefab de la plataforma custom que sigue una ruta de waypoints.
    Se ha añadido tambien el paquete WaypointSystem.

---
+++ Assets/MyAssets/Scripts/Platforms/PingPongPlatform.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PingPongPlatform : StaticPlatform {
+
+	/*AQUI EL CODIGO DE MOVING_PLATFORM *EXCEPTO* LOS COLLIDERS QUE ESO SE HEREDA DE StaticPlatform
+	*aunque el mismo efecto puede conseguirse facilmente con las custom.
+	**/
+}
---
+++ Assets/MyAssets/Scripts/Platforms/StaticPlatform.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaticPlatform : MonoBehaviour {
+
+	void OnCollisionEnter(Collision other){
+		Debug.Log ("hit on platform");
+		if(other.gameObject.tag == "Player"){
+
+
+			other.transform.parent = this.transform;
+		}
+		
+	}
+	
+	void OnCollisionExit(Collision other){
+		if(other.gameObject.tag == "Player"){
+			other.transform.parent = null;
+		}
+	}
+}
