d7124b43d4318db7d0567013a72d4a6287b0ed8f
Eldir <lothvorn@gmail.com>
2015-05-04 23:24:23 +0200

    Trabajando con las interacciones
    Se han creado los scripts base para la jerarquia de un sistema de
    interacción con el escenario que funcione con el mecanismo de 'quedarse
    mirando a un objeto y activarlo pasado x segundos'

---
+++ Assets/MyAssets/Scripts/Interaction/InteractionAgent.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractionAgent : MonoBehaviour {
+
+	//keep public
+	public float activationTime;
+
+	//to hide
+	public bool isSelected = false;
+
+	//to set private
+	public float activationTimer= 0f;
+
+	void Update (){
+		if ( isSelected){
+			activationTimer += Time.deltaTime;
+
+			if (activationTimer >= activationTime)
+				MakeActions();
+		}
+	}
+
+
+	public void Select (){
+		isSelected = true;
+	}
+
+	public void Unselect (){
+		isSelected = false;
+		activationTimer = 0f;
+	}
+
+	void MakeActions (){
+		Debug.Log (transform.name + " acting");
+	}
+}
--- Assets/MyAssets/Scripts/Interaction/InteractorTool.cs
+++ Assets/MyAssets/Scripts/Interaction/InteractorTool.cs
@@ -3,13 +3,60 @@ using System.Collections;
 
 public class InteractorTool : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public float interactionDistance;
+	//public GameObject lastSelected = null;
+	public InteractionAgent lastSelected = null;
+
+
+	//references to other elements in the same prefab
+	public Camera playerCamera;
+
+
+	//to set private
+	public InteractionAgent currentAgent;
+
+	void Update() {
+		RaycastHit hit;
+
+		Debug.DrawRay (playerCamera.transform.position, playerCamera.transform.forward*interactionDistance, Color.cyan);
+		Physics.Raycast (playerCamera.transform.position, playerCamera.transform.forward, out hit, interactionDistance);
+
+		if (hit.transform != null && hit.transform.tag == "InteractiveByRay"){
+			currentAgent = hit.transform.GetComponent<InteractionAgent>();
+
+		} else{
+			currentAgent = null;
+		}
+
+
+		if ( currentAgent != lastSelected){
+			if (currentAgent == null){
+				lastSelected.Unselect();
+				lastSelected = null;
+			} else{
+				if (lastSelected != null)
+					lastSelected.Unselect ();
+				lastSelected = currentAgent;
+				lastSelected.Select();
+			}
+
+
+		}
+
+/*
+		if (hit.transform != null && hit.transform.tag == "InteractiveByRay"){
+			InteractionAgent currentAgent = hit.transform.GetComponent<InteractionAgent>();
+			if (currentAgent != lastSelected){
+				if (lastSelected != null){
+					lastSelected.Unselect();
+				}
+				lastSelected = currentAgent;
+				lastSelected.Select();
+				//InteractionAgent interactionAgent = hit.transform.GetComponent<InteractionAgent>();
+			}
+
+		
+		}
+*/
 	}
 }
