e02a65cc5a83e78d9ab55945873fef40c7630e58
lothvorn <lothvorn@gmail.com>
2015-05-25 21:40:44 +0200

    Prototipo de 'movimiento camara automatico'

---
+++ Assets/MyAssets/Scripts/EventsControllers/AutoCamera/AutoCameraMotionController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoCameraMotionController : MonoBehaviour {
+	public GameObject autoCamera;
+	public GameObject autoCameraTarget;
+	public GameObject fpsController;
+
+	public float stopDistance = 0.5f;
+	bool fadeIn = false;
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance (autoCamera.transform.position,autoCameraTarget.transform.position)<stopDistance){
+			//fade out auto camera
+			//fade in fpscontroller camera
+		}
+
+		if (fadeIn){
+			//enable fpsController
+			//disable this
+		}
+
+	}
+
+	/*
+	void FadeIn(){
+//do fade in
+//set fadedIn flag = true
+	}
+
+*/
+}
