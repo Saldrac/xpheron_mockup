08440d9d8bbbbe2bf7ba14d7338a6fbe8aaaf48e
lothvorn <lothvorn@gmail.com>
2015-05-28 20:16:38 +0200

    Arreglos menores en el movimiento automatico de camaras

--- Assets/MyAssets/Scripts/EventsControllers/AutoCamera/AutoCameraMotionController.cs
+++ Assets/MyAssets/Scripts/EventsControllers/AutoCamera/AutoCameraMotionController.cs
@@ -1,8 +1,12 @@
 ﻿//este script es para pruebas. Idealmente esta funcionalidad estara incluida en el sistema de gestion de eventos
 //y el 'cameraAutoTracking' (la funcionalidad que ofrece este script) heredara de la clase base 'evento'
 
+
+//devuelve el control al fps controller
 using UnityEngine;
 using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
 
 public class AutoCameraMotionController : MonoBehaviour {
 	public GameObject autoCamera;
@@ -10,11 +14,23 @@ public class AutoCameraMotionController : MonoBehaviour {
 	public GameObject fpsController;
 
 	public float stopDistance = 0.5f;
+
+	public float timer = 5;
 	bool fadeIn = false;
 
+
+	void Awake (){
+		fpsController.GetComponent<RigidbodyFirstPersonController>().enabled = false;
+	}
 	
 	// Update is called once per frame
 	void Update () {
+/*
+		if (timer > 0)
+			return;
+*/
+		timer -= Time.deltaTime;
+
 		if (Vector3.Distance (autoCamera.transform.position,autoCameraTarget.transform.position)<stopDistance){
 			//fade out auto camera
 			//fade in fpscontroller camera
@@ -24,6 +40,7 @@ public class AutoCameraMotionController : MonoBehaviour {
 		if (fadeIn){
 			//enable fpsController
 			//disable this
+			fpsController.GetComponent<RigidbodyFirstPersonController>().enabled = true;
 			fpsController.SetActive (true);
 			autoCamera.transform.parent.gameObject.SetActive(false);
 		}
---
+++ Assets/MyAssets/Scripts/ZZTemp/TempOrchestrator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TempOrchestrator : MonoBehaviour {
+
+	public float timer = 5f;
+
+	public GameObject autoCamera;
+	//public GameObject mainFPS;
+	// Use this for initialization
+	void Start () {
+
+		//set initial status
+		autoCamera.SetActive (false);
+		//mainFPS.SetActive (false);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer > 0 ){
+			timer -= Time.deltaTime;
+			return;
+		}
+
+		//mainFPS.SetActive (true);
+		autoCamera.SetActive (true);
+		enabled = false;
+
+	}
+}
