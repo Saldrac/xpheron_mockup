0bb1eec74afed3debce34286fde437fa6a1003ff
lothvorn <lothvorn@gmail.com>
2015-06-01 21:53:51 +0200

    Funcionalidad puerta terminada
    La puerta ya funciona. Se ha quitado el script del package y se ha
    rehecho de una forma mucho mas racional.

---
+++ Assets/MyAssets/Scripts/Prefabs/SphereElements/SphereDoors.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SphereDoors : MonoBehaviour {
+
+	public Animator doorTop;
+	public Animator doorBottom;
+	public BoxCollider trigger;
+
+	//to set private
+	public int playersInTheTrigger=0;
+
+
+	public void Update (){
+		if (playersInTheTrigger >= 1){
+			doorTop.SetBool("Opening",true);
+			doorBottom.SetBool("Opening",true);
+
+		} else{
+			doorTop.SetBool("Opening",false);
+			doorBottom.SetBool("Opening",false);
+		}
+	}
+
+
+	public void OnTriggerEnter (Collider other){
+		if (other.gameObject.tag == "Player"){
+			playersInTheTrigger ++;
+		}
+	}
+
+	public void OnTriggerExit (Collider other){
+		if (other.gameObject.tag == "Player"){
+			playersInTheTrigger --;
+		}
+	}
+	
+}
