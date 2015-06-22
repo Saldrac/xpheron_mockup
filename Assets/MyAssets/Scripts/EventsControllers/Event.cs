dd18967d190e93c4384d5c40fd6f97eb9df3d667
lothvorn <lothvorn@gmail.com>
2015-06-22 20:18:59 +0200

    Sistema de eventos
    Iniciada la implementacion del sistema de eventos

---
+++ Assets/MyAssets/Scripts/EventsControllers/Event.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Event : MonoBehaviour {
+	public GameObject [] objectsToEnable;
+	public GameObject [] objectsToDisable;
+
+	public void Start(){
+		LaunchEvent ();
+	}
+
+
+	public void LaunchEvent(){
+
+		UpdateObjectsStatus();
+		Debug.Log ("Evento " +transform.name + " lanzado");
+	}
+
+
+
+
+	private void UpdateObjectsStatus(){
+		if (objectsToEnable != null){
+			for (int i =0; i < objectsToEnable.Length; i ++){
+				objectsToEnable[i].SetActive(true);
+			}
+		}
+
+		if (objectsToDisable != null){
+			for (int i = 0; i < objectsToDisable.Length; i++){
+				objectsToDisable[i].SetActive (false);
+			}
+		}
+	}
+}
