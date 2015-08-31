3bd191d894baf786a65c75cde567cf671dc07708
lothvorn <lothvorn@gmail.com>
2015-09-01 00:21:10 +0200

    efecto deslumbrado listo
    animado y sincronizado con el evento de la holodeck (como parte del
    evento de activar. Solo modifica la luz principal, faltaria hacer algo
    parecido con el skybox)

--- Assets/MyAssets/Scripts/EventsControllers/GeneralUseEvents/EVA_ActivateHolodeck.cs
+++ Assets/MyAssets/Scripts/EventsControllers/GeneralUseEvents/EVA_ActivateHolodeck.cs
@@ -4,11 +4,49 @@ using System.Collections;
 public class EVA_ActivateHolodeck : EventAgent {
 
 	public Animator holodeckAnimator;
+	public Animator platformsAnimator;
+	public Animator lightEffectAnimation;
+
+	public GameObject platformsSet; //to hide until animate
+	//to set private
+	public float generalTimer;
+	public float platformsTime =3;
+	public bool waitingForPlatforms = false;
+
+	public float deslumbrarTime = 2;
+	public bool deslumbrarHecho = false;
+	public void Update(){
+		if (!waitingForPlatforms)
+			return;
+
+		//las plataformas se activan retardadas respecto a la holodeck
+		generalTimer+=Time.deltaTime;
+		if (generalTimer >= platformsTime){
+			platformsSet.SetActive(true);
+			platformsAnimator.SetBool ("Activate", true);
+			platformsAnimator.SetBool ("Deactivate",false);
+			waitingForPlatforms = false;
+		}
+
+		if (!deslumbrarHecho){
+			if (generalTimer>= deslumbrarTime){
+				deslumbrarHecho = true;
+				lightEffectAnimation.SetBool ("Deslumbrar",true);
+			}
+		}
+
+
+
+	}
 
 	public override void PerformEventActions ()
 	{
 		holodeckAnimator.SetBool ("Activate",true);
 		holodeckAnimator.SetBool ("Deactivate",false);
+		platformsSet.SetActive(false);
+		waitingForPlatforms = true;
+
+
 		//Debug.Log ("inicializando holodeck");
 	}
 }
